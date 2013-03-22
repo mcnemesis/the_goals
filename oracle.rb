843ad19b2823ed9ca0a8db350c91ed6668bda1de
Joseph Lutalo W <jlutalo@ds.co.ug>
2013-03-22 20:41:01 +0300

    The Oracle needs no ReadMe

--- oracle.rb
+++ oracle.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-d=File.expand_path(File.dirname(__FILE__));l=Dir.entries(d).select{|s| s=~/^[^\.o]/ };print `cat #{"#{d}/#{l[rand l.length]}"}`
+d=File.expand_path(File.dirname(__FILE__));l=Dir.entries(d).select{|s| s=~/^[^\.oR]/ };print `cat #{"#{d}/#{l[rand l.length]}"}`
